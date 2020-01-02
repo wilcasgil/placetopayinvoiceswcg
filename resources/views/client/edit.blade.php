3a8a0fd332a36d9020d646c358fccdd96393bde9
wilcasgil <c.wilcas@gmail.com>
2020-01-01 23:17:51 -0500

    Complete crud

--- resources/views/client/edit.blade.php
+++ resources/views/client/edit.blade.php
@@ -3,12 +3,12 @@
 @section('content')
     <div class="row">
         <div class="col">
-            <h1>Edit Client {{ $client->id }}</h1>
+            <h1>Edit client {{ $client->id }}</h1>
         </div>
     </div>
     <div class="row">
         <div class="col">
-            <a class="btn btn-secondary" href="/clients">Back</a>
+            <a class="btn btn-secondary" href="{{ route('clients.index') }}">Back</a>
         </div>
     </div>
     <br>
@@ -23,20 +23,20 @@
                     </ul>
                 </div>
             @endif
-            <form action="/clients/{{ $client->id }}" method="POST">
+            <form action="{{ route('clients.update', $client) }}" method="POST">
                 @csrf
-                @method('put')
+                @method('PATCH')
                 <div class="form-group">
                     <label for="name">Name:</label>
-                    <input type="text" class="form-control" id="name" name="name" placeholder="Type a client name" value="{{ old('name') }}">
+                    <input type="text" class="form-control" id="name" name="name" placeholder="Type a client name" value="{{ $client->name }}" required>
                 </div>
                 <div class="form-group">
                     <label for="last_name">Last Name:</label>
-                    <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Type a client last name" value="{{ old('last_name') }}">
+                    <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Type a client last name" value="{{ $client->last_name }}" required>
                 </div>
                 <div class="form-group">
                     <label for="email">Email:</label>
-                    <input type="text" class="form-control" id="email" name="email" placeholder="Type a client email" value="{{ old('email') }}">
+                    <input type="text" class="form-control" id="email" name="email" placeholder="Type a client email" value="{{ $client->email }}">
                 </div>
                 <button class="btn btn-primary" type="submit">Submit</button>
             </form>
