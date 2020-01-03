a56fbe39aacac532b6f4274cdfc62ec34492495e
wilcasgil <c.wilcas@gmail.com>
2020-01-03 13:13:08 -0500

    Login completed

--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Auth::routes();
+Auth::routes();
 
-Route::resource('clients', 'ClientController');
-Route::get('/clients/{id}/confirmDelete', 'ClientController@confirmDelete');
+Route::middleware(['auth'])->group(function (){
+    Route::resource('clients', 'ClientController');
+    Route::get('/clients/{id}/confirmDelete', 'ClientController@confirmDelete');
 
-Route::resource('categories', 'CategoryController');
-Route::get('/categories/{id}/confirmDelete', 'CategoryController@confirmDelete');
+    Route::resource('categories', 'CategoryController');
+    Route::get('/categories/{id}/confirmDelete', 'CategoryController@confirmDelete');
 
-Route::resource('subcategories', 'SubcategoryController');
-Route::get('/subcategories/{id}/confirmDelete', 'SubcategoryController@confirmDelete');
+    Route::resource('subcategories', 'SubcategoryController');
+    Route::get('/subcategories/{id}/confirmDelete', 'SubcategoryController@confirmDelete');
 
-Route::resource('paymentTypes', 'PaymentTypeController');
-route::get('paymentTypes/{id}/confirmDelete', 'PaymentTypeController@confirmDelete');
+    Route::resource('paymentTypes', 'PaymentTypeController');
+    route::get('paymentTypes/{id}/confirmDelete', 'PaymentTypeController@confirmDelete');
 
-Route::resource('invoiceStates', 'InvoiceStateController');
-route::get('invoiceStates/{id}/confirmDelete', 'InvoiceStateController@confirmDelete');
+    Route::resource('invoiceStates', 'InvoiceStateController');
+    route::get('invoiceStates/{id}/confirmDelete', 'InvoiceStateController@confirmDelete');    
+
+    Route::get('/home', 'HomeController@index')->name('home');
+});
