7a8d8efc170f60819626660651585d05952741e5
wilcasgil <c.wilcas@gmail.com>
2020-01-26 21:49:24 -0500

    New files for countries, model, migration, factory and test

---
+++ tests/Unit/CountryTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Country;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+//use PHPUnit\Framework\TestCase;
+
+class CountryTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testCreateCountry()
+    {
+        $country = factory(Country::class)->create();
+        
+        $this->assertEquals('/country/' . $country->name,
+                                          $country->active);
+    }
+}
