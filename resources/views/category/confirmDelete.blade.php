4f2236db760ee6a51ed200620e00fa2e8470fb66
wilcasgil <c.wilcas@gmail.com>
2020-02-02 19:51:44 -0500

    ok categories index text and new confirm delete categories test file

--- resources/views/category/confirmDelete.blade.php
+++ resources/views/category/confirmDelete.blade.php
@@ -10,9 +10,9 @@
 
 <div class="row">
     <div class="col">
-        <a class="btn btn-secondary" href="{{ route('categories.index') }}">Back</a>
+        <a class="btn btn-secondary" href="{{ route('categories.index') }}">{{ __('Back') }}</a>
 
-        <a class="btn btn-secondary" href="{{ route('categories.edit', $category) }}">Edit</a>
+        <a class="btn btn-secondary" href="{{ route('categories.edit', $category) }}">{{ __('Edit') }}</a>
     </div>
 </div>
 <br>
@@ -21,15 +21,15 @@
     <h3>Details</h3>
     <table class="table table-hover table-sm">
         <thead>
-            <th>Id</th>
-            <th>Created at</th>
-            <th>Updated at</th>
+            <th>{{ __('Id') }}</th>
+            <th>{{ __('Created at') }}</th>
+            <th>{{ __('Updated at') }}</th>
             <th class="text-right"></th>
         </thead>
 
         <tbody>
             <tr>
-                <td>{{ $category->id }}</td>
+                <td>{{ __($category->id) }}</td>
                 <td>{{ $category->created_at }}</td>
                 <td>{{ $category->updated_at }}</td>
             </tr>
@@ -43,7 +43,7 @@
         <form action="/categories/{{ $category->id }}" method="POST">
             @csrf
             @method('delete')
-            <button class="btn btn-primary" type="submit">Delete</button>
+            <button class="btn btn-primary" type="submit">{{ __('Delete') }}</button>
         </form>
     </div>
 </div>
